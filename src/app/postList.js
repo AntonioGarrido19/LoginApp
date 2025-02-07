1e8230c941c9602fe79a6fd4240d0bc0162301ba
AntonioGarrido19 <anto.garrido.98@gmail.com>
2025-02-07 16:11:42 -0300

    cambios en post

--- src/app/postList.js
+++ src/app/postList.js
@@ -1,24 +1,22 @@
-const postList = document.querySelector('.posts')
-
+const postList = document.querySelector(".posts");
 
 export const setupPosts = (data) => {
-    if (data.length) {
-     let html = ""
+  if (data.length) {
+    let html = "";
 
-        data.forEach(doc => {
-        const post = doc.data()
-        console.log(post);
-        const li = 
-        `<li class="list-group-item list-group-item-action list-group-item-dark" >
+    data.forEach((doc) => {
+      const post = doc.data();
+      console.log(post);
+      const li = `<li class="list-group-item list-group-item-action list-group-item-dark" >
         <h5>${post.title}</h5>
         <p>${post.content}</p>
-        </li>`
+        </li>`;
 
-        html += li
-        })
-        postList.innerHTML = html
-        console.log(postList);
-    } else {
-        postList.innerHTML = '<h1> Log in to see posts </h1>'
-    }
-}
+      html += li;
+    });
+    postList.innerHTML = html;
+    console.log(postList);
+  } else {
+    postList.innerHTML = "<h1> You dont have rated any movies yet </h1>";
+  }
+};
