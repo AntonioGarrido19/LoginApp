3d52c77749e639564f4cfa2209cb97e6120883c3
AntonioGarrido19 <anto.garrido.98@gmail.com>
2025-02-07 19:12:19 -0300

    api connected

--- src/app/postList.js
+++ src/app/postList.js
@@ -1,21 +1,54 @@
 const postList = document.querySelector(".posts");
 
+// export const setupPosts = (data) => {
+//   if (data.length) {
+//     let html = "";
+
+//     data.forEach((doc) => {
+//       const post = doc.data();
+//       console.log(post);
+//       const li = `<li class="list-group-item list-group-item-action list-group-item-dark" >
+//         <h5>${post.title}</h5>
+//         <p>${post.content}</p>
+//         </li>`;
+
+//       html += li;
+//     });
+//     postList.innerHTML = html;
+//     console.log(postList);
+//   } else {
+//     postList.innerHTML = "<h1> There is no movies posted </h1>";
+//   }
+// };
+
 export const setupPosts = (data) => {
   if (data.length) {
-    let html = "";
-
-    data.forEach((doc) => {
-      const post = doc.data();
-      console.log(post);
-      const li = `<li class="list-group-item list-group-item-action list-group-item-dark" >
-        <h5>${post.title}</h5>
-        <p>${post.content}</p>
-        </li>`;
-
-      html += li;
-    });
-    postList.innerHTML = html;
-    console.log(postList);
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=d6695246ed8b602e5bf1d2d12995fd00",
+      data
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        data.results.forEach((movie) => {
+          const movieElement = document.createElement("div");
+          movieElement.classList.add("movie");
+          movieElement.innerHTML = `
+          <div>
+            <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="${movie.title}">
+            </div>
+            <div>
+            <h3>${movie.title}</h3>
+            <p>${movie.overview}</p>
+            <input type="button" value="Agregar a mi Lista">
+            </div>
+          
+        `;
+          postList.appendChild(movieElement);
+        });
+      })
+      .catch((error) =>
+        console.error("Error al obtener las películas:", error)
+      );
   } else {
     postList.innerHTML = "<h1> There is no movies posted </h1>";
   }
