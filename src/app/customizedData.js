0efe981bc4dd3a1985441e3f1f14ae9c36afb840
AntonioGarrido19 <anto.garrido.98@gmail.com>
2025-02-10 15:34:39 -0300

    changes customized data

--- src/app/customizedData.js
+++ src/app/customizedData.js
@@ -1,29 +1,7 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 
-const postListC = document.querySelector(".custom");
 const profileData = document.querySelector("#profile");
-
-export const setupCustomized = (dataC) => {
-  if (dataC.length) {
-    let html = "";
-
-    dataC.forEach((doc) => {
-      const post = doc.data();
-      console.log(post);
-      const li = `<li class="list-group-item list-group-item-action list-group-item-dark" >
-        <h5>${post.name}</h5>
-        <p>${post.data}</p>
-        </li>`;
-
-      html += li;
-    });
-    postListC.innerHTML = html;
-    console.log(postListC);
-  } else {
-    postListC.innerHTML = "<h1> You dont have rated any movies yet </h1>";
-  }
-};
 
 // Detectar usuario logeado
 onAuthStateChanged(auth, (user) => {
--- src/app/postList.js
+++ src/app/postList.js
@@ -1,26 +1,5 @@
 const postList = document.querySelector(".posts");
 
-// export const setupPosts = (data) => {
-//   if (data.length) {
-//     let html = "";
-
-//     data.forEach((doc) => {
-//       const post = doc.data();
-//       console.log(post);
-//       const li = `<li class="list-group-item list-group-item-action list-group-item-dark" >
-//         <h5>${post.title}</h5>
-//         <p>${post.content}</p>
-//         </li>`;
-
-//       html += li;
-//     });
-//     postList.innerHTML = html;
-//     console.log(postList);
-//   } else {
-//     postList.innerHTML = "<h1> There is no movies posted </h1>";
-//   }
-// };
-
 export const setupPosts = (data) => {
   if (data.length) {
     fetch(
@@ -36,11 +15,10 @@ export const setupPosts = (data) => {
          
 
             <div class="card" style="width: 14rem;">
-  <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" class="card-img-top img-fluid h-50" alt="${movie.title}">
-  <div class="card-body text-center">
-    <h5 class="card-title">${movie.title}</h5>
-  </div>
-</div>
+                <a href="singleMovie.html?id=${movie.id}">
+                <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" class="card-img-top img-fluid h-50" alt="${movie.title}">
+              </a>
+            </div>
           
         `;
           postList.appendChild(movieElement);
